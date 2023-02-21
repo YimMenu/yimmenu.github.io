f7ffab6ccd32b27dcab07295d099cd2c4eac1e3d
Evangelos Paterakis <evan@geopjr.dev>
2023-02-21 15:42:31 +0200

    chore: .template => .template.html

--- .github/replace.js
+++ .github/replace.js
@@ -1,3 +1,3 @@
 const fs = require("fs")
 
-fs.writeFileSync("changelog.html", fs.readFileSync("changelog.template", { encoding: "utf-8" }).replace("{{ K25GGECMBaKQfj&B#dorq8HpCCTkdEwSa!@hLVHHoWriEVBnrmL*ty%rHooB*esezywBQ@sN8tKJ9YBR^FLr8J@28hyxm#$Vd8KG7VEBdCi2FNSC7sH2uiFwdCTWWzbi }}", fs.readFileSync("changelog_from_md.html", { encoding: "utf8" })))
+fs.writeFileSync("changelog.html", fs.readFileSync("changelog.template.html", { encoding: "utf-8" }).replace("{{ K25GGECMBaKQfj&B#dorq8HpCCTkdEwSa!@hLVHHoWriEVBnrmL*ty%rHooB*esezywBQ@sN8tKJ9YBR^FLr8J@28hyxm#$Vd8KG7VEBdCi2FNSC7sH2uiFwdCTWWzbi }}", fs.readFileSync("changelog_from_md.html", { encoding: "utf8" })))
